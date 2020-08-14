6618040365e66add6a19a9baa5ab2d553ff91a57
rzhai2 <rzhai2@jh.edu>
2020-08-14 13:45:40 -0400

    fixed database for Q2A4h

--- app/src/main/java/com/jhu/chenyuzhang/experimentgame/Questions/Question2Att4OpHorizontal.java
+++ app/src/main/java/com/jhu/chenyuzhang/experimentgame/Questions/Question2Att4OpHorizontal.java
@@ -64,6 +64,7 @@ public class Question2Att4OpHorizontal extends AppCompatActivity {
     private String eventDisplay = "Displayed";
     private String eventTimeOut = "TimeOut, Covered";
     private String dbTstamp;
+    private String not_covered = "";
 
     private long backPressedTime;
     private long startTime;
@@ -337,6 +338,7 @@ public class Question2Att4OpHorizontal extends AppCompatActivity {
 
                 if (checkMinimumTimePassed()) {
                     unmaskAttributes(new ViewAnimator[]{viewAnimator11, viewAnimator12});
+                    recordEvent("Option1 Mask Off");
                     showResult(a1, 1);
                 }
             }
@@ -355,6 +357,7 @@ public class Question2Att4OpHorizontal extends AppCompatActivity {
 
                 if (checkMinimumTimePassed()) {
                     unmaskAttributes(new ViewAnimator[]{viewAnimator21, viewAnimator22});
+                    recordEvent("Option2 Mask Off");
                     showResult(a2, 2);
                 }
             }
@@ -373,6 +376,7 @@ public class Question2Att4OpHorizontal extends AppCompatActivity {
 
                 if (checkMinimumTimePassed()) {
                     unmaskAttributes(new ViewAnimator[]{viewAnimator31, viewAnimator32});
+                    recordEvent("Option3 Mask Off");
                     showResult(a3, 3);
                 }
             }
@@ -392,6 +396,7 @@ public class Question2Att4OpHorizontal extends AppCompatActivity {
 
                 if (checkMinimumTimePassed()) {
                     unmaskAttributes(new ViewAnimator[]{viewAnimator41, viewAnimator42});
+                    recordEvent("Option4 Mask Off");
                     showResult(a4, 4);
                 }
             }
@@ -403,6 +408,7 @@ public class Question2Att4OpHorizontal extends AppCompatActivity {
         /* on tap, if the attribute view is covered, uncover it for 1s and cover other attributes */
         if (tappedView.getDisplayedChild() == 0) {
             final String[] codes = identifiers.get(tappedView.getId()); // get the corresponding identifiers for the clicked attribute
+
             dbTstamp = recordEvent(codes[2] + ", " + codes[3] + " " + eventClick);
             /* Bluetooth
             try {
@@ -411,12 +417,30 @@ public class Question2Att4OpHorizontal extends AppCompatActivity {
             } catch (IOException e) {}
 
              */
+            if (!not_covered.equals("")) {
+                /* if other attributes are uncovered, cover them */
+                for (ViewAnimator v: otherViews) {
+                    if (v.getDisplayedChild() == 1) {
+                        dbTstamp = recordEvent(not_covered +  " Early Mask On");
+                        not_covered = "";
+                    /* Bluetooth
+                    try {
+                        bluetooth.timeStamper( identifier_coverEarly, dbTstamp);
+                    } catch (IOException e) {}
+
+                     */
+
+                        v.showNext();
+                    }
+                }
+            }
 
 
             //armVSyncHandlerA1();
 
             tappedView.showNext();  /* uncover */
             dbTstamp = recordEvent(codes[2] + ", " + codes[3] + " " + eventDisplay);
+            not_covered = codes[2] + ", " + codes[3];
             /* Bluetooth
             try {
                 bluetooth.timeStamper( codes[1], dbTstamp);
@@ -434,6 +458,7 @@ public class Question2Att4OpHorizontal extends AppCompatActivity {
                     if (tappedView.getDisplayedChild() == 1) {
                         tappedView.showNext();
                         dbTstamp = recordEvent(codes[2] + ", " + codes[3] + " " + eventTimeOut);
+                        not_covered = "";
                         /* Bluetooth
                         try {
                             bluetooth.timeStamper( identifier_cover, dbTstamp);
@@ -447,20 +472,6 @@ public class Question2Att4OpHorizontal extends AppCompatActivity {
             }, 1000);
             viewHandlerMap.put(tappedView.getId(), handler);
 
-            /* if other attributes are uncovered, cover them */
-            for (ViewAnimator v: otherViews) {
-                if (v.getDisplayedChild() == 1) {
-                    dbTstamp = recordEvent(codes[2] + " " + codes[3] +  " Early Mask On");
-                    /* Bluetooth
-                    try {
-                        bluetooth.timeStamper( identifier_coverEarly, dbTstamp);
-                    } catch (IOException e) {}
-
-                     */
-
-                    v.showNext();
-                }
-            }
         }
 
         countDownTimer.cancel();
