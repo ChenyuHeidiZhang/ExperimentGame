c83065473dbb5fa6ae997f358d525b125ec7119e
rzhai2 <rzhai2@jh.edu>
2020-10-11 13:58:14 -0400

    delete the timeRecordDb.close from ResultActivity

--- app/src/main/java/com/jhu/chenyuzhang/experimentgame/ResultActivity.java
+++ app/src/main/java/com/jhu/chenyuzhang/experimentgame/ResultActivity.java
@@ -298,7 +298,7 @@ public class ResultActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         if (backPressedTime + 2000 > System.currentTimeMillis()) {
-            timeRecordDb.close();
+            //timeRecordDb.close();
             finish();
         } else {
             Toast.makeText(this, "Press back again to finish", Toast.LENGTH_SHORT).show();
