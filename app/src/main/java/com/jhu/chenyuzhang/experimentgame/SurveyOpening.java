6530de6eb8d07283077bb3c76e5c44e6764c3062
rzhai2 <rzhai2@jh.edu>
2021-02-05 16:04:58 -0500

    added a little bit of the survey

---
+++ app/src/main/java/com/jhu/chenyuzhang/experimentgame/SurveyOpening.java
@@ -0,0 +1,13 @@
+package com.jhu.chenyuzhang.experimentgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SurveyOpening extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_survey_opening);
+    }
+}
